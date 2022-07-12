d9ed3be47534dd1d64ca7a201bc17bf274588614
sergio.fontecha <sergiofontecha@MacBook-Pro-de-ApiumHub-2.local>
2022-07-12 12:12:00 +0200

    develop: First contact with Vue framework

---
+++ singleFileComponentExample.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <todo-item
+      v-for="(todo, index) in todoList"
+      :key="index"
+      :todo="todo"
+    ></todo-item>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  props: ['todoList'],
+  methods: {},
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+</style>
