f2b0000ad5888b0a9f5c7a54620304f761b33c2d
sergio.fontecha <sergiofontecha@MacBook-Pro-de-ApiumHub-2.local>
2022-07-13 16:42:03 +0200

    App, Header and Footer components created

---
+++ vue-trello/src/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <span>Esto es footer</span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "app-footer"
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  box-sizing: border-box;
+  background-color: #cfd8dc;
+  bottom: 0;
+  left: 0;
+  padding: 1rem;
+  position: absolute;
+  width: 100%;
+}
+</style>
