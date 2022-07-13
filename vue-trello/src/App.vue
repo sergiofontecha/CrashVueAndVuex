e0896af6e8b1432dcc3315aa92417302d7836cce
sergio.fontecha <sergiofontecha@MacBook-Pro-de-ApiumHub-2.local>
2022-07-13 16:46:58 +0200

    develop: solving naming errors

--- vue-trello/src/App.vue
+++ vue-trello/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <app-header></app-header>
     <router-view class="container" />
-    <footer></footer>
+    <app-footer></app-footer>
   </div>
 </template>
 
--- vue-trello/src/components/AppHeader.vue
+++ vue-trello/src/components/AppHeader.vue
@@ -7,7 +7,7 @@
 
 <script>
 export default {
-  name: "app-head"
+  name: "app-header"
 };
 </script>
 
